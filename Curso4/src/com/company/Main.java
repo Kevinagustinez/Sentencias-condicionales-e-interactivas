5014936bae407f6dd98e04c38cf2e8d873eacfed
Kevin <Kevinagustinez@gmail.com>
2022-07-16 11:20:59 -0300

    Ejercicio 1 con metodo en interactivos

--- Curso4/src/com/company/Main.java
+++ Curso4/src/com/company/Main.java
@@ -3,8 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int i=0;
+        interactivosEx1();
+    }
 
+    private static void interactivosEx1() {
+        int i=0;
+        imprimirFoo(i);
+    }
+
+    private static void imprimirFoo(int i) {
         while(i<10){
             System.out.println("foo");
             i++;
