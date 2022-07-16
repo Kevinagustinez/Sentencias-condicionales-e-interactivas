ac4d0e07e70fb5c8fc226b27f5073e64895dcc99
Kevin <Kevinagustinez@gmail.com>
2022-07-16 11:22:34 -0300

    Ejercicio 2 con metodo en interactivos

--- Curso4/src/com/company/Main.java
+++ Curso4/src/com/company/Main.java
@@ -3,7 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        interactivosEx2();
+    }
+
+    private static void interactivosEx2() {
         int i=0;
+        imprimirSec(i);
+    }
+
+    private static void imprimirSec(int i) {
         for(i=1; i<11; i++){
             System.out.print(" "+ i +" ");
         }
