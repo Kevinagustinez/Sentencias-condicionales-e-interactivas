f16dbf2bbc446c02445af340ccaf785f11f7793f
Kevin <Kevinagustinez@gmail.com>
2022-07-16 10:49:16 -0300

    Ejercicio 1 de condicionales

--- Curso4/src/com/company/Main.java
+++ Curso4/src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int num_uno=0;
+        int num_dos=0;
+        Scanner in= new Scanner(System.in);
+
+        System.out.println("Dame el primer numero para tu comparacion ");
+        num_uno= in.nextInt();
+
+        System.out.println("Dame el segundo numero para tu comparacion ");
+        num_dos= in.nextInt();
+
+        if (num_uno>num_dos){
+            System.out.println(num_uno + " es el numero mayor");
+        }else if (num_uno<num_dos){
+            System.out.println(num_dos + " es el numero mayor");
+        }else{
+            System.out.println("Ambos numeros son iguales");
+        }
     }
 }
