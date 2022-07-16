1c60bb732e82caad33025772f52b350cea6503b0
Kevin <Kevinagustinez@gmail.com>
2022-07-16 11:00:05 -0300

    Ejercicio 3 de interactivos

--- Curso4/src/com/company/Main.java
+++ Curso4/src/com/company/Main.java
@@ -3,6 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String asterisco="";
+        for (int i = 0; i < 7; i++) {
+            asterisco += "*";
+            System.out.println(asterisco);
+        }
+        System.out.println("");
+        for (int j= 0; j < 7; j++) {
+            asterisco = "";
+            for (int i = 0; i < 7 - j; i++) {
+                asterisco += "*";
+            }
+            System.out.println(asterisco);
+        }
+        System.out.println("");
+        for (int j= 0; j < 7; j++) {
+            asterisco = "";
+            for (int i = 0; i < 7 - j - 1; i++) {
+                asterisco += " ";
+            }
+            for (int i = 0; i < j + 1; i++) {
+                asterisco += "*";
+            }
+            System.out.println(asterisco);
+
+        }
     }
 }
