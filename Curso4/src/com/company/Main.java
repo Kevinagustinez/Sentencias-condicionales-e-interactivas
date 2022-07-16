7a452bf5ce54f2cadf39d6ea746becbc4642aebe
Kevin <Kevinagustinez@gmail.com>
2022-07-16 11:23:42 -0300

    Ejercicio 3 con metodo en interactivos

--- Curso4/src/com/company/Main.java
+++ Curso4/src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        interactivosEx3();
+    }
+
+    private static void interactivosEx3() {
         String asterisco="";
         for (int i = 0; i < 7; i++) {
             asterisco += "*";
@@ -26,7 +30,6 @@ public class Main {
                 asterisco += "*";
             }
             System.out.println(asterisco);
-
         }
     }
 }
