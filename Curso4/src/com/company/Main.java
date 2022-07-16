5f435f5755830df91319c02d062c7657146a5c7d
Kevin <Kevinagustinez@gmail.com>
2022-07-16 11:03:46 -0300

    Ejercicio 1 con metodo condicionales

--- Curso4/src/com/company/Main.java
+++ Curso4/src/com/company/Main.java
@@ -5,16 +5,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int num_uno=0;
-        int num_dos=0;
+
+        condicionalesEx1();
+    }
+
+    private static void condicionalesEx1() {
         Scanner in= new Scanner(System.in);
+        int num_uno = ingresarInt(in, "Dame el primer numero para tu comparacion ");
+        int num_dos= ingresarInt(in, "Dame el segundo numero para tu comparacion");
+        numerosInt(num_uno , num_dos);
+    }
 
-        System.out.println("Dame el primer numero para tu comparacion ");
-        num_uno= in.nextInt();
-
-        System.out.println("Dame el segundo numero para tu comparacion ");
-        num_dos= in.nextInt();
-
+    private static int ingresarInt(Scanner lector, String text) {
+        System.out.println(text);
+        return lector.nextInt();
+    }
+    private static void numerosInt(int num_uno, int  num_dos) {
         if (num_uno>num_dos){
             System.out.println(num_uno + " es el numero mayor");
         }else if (num_uno<num_dos){
