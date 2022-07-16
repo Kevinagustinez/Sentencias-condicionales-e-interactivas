ccf684a5c39b431b1c3f186744a7b0c00d649cee
Kevin <Kevinagustinez@gmail.com>
2022-07-16 10:55:51 -0300

    Ejercicio 1 en interactivos

--- Curso4/src/com/company/Main.java
+++ Curso4/src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int i=0;
+
+        while(i<10){
+            System.out.println("foo");
+            i++;
+        }
     }
 }
