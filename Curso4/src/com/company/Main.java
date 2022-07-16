80e34aa742d4ed96225d9dd9222e12762f454af1
Kevin <Kevinagustinez@gmail.com>
2022-07-16 10:54:13 -0300

    Ejercicio 3 en condicionales

--- Curso4/src/com/company/Main.java
+++ Curso4/src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        double Matematicas = 0d;
+        double Biologia = 0d;
+        double Quimica = 0d;
+        double promedio = 0d;
+        Scanner in = new Scanner(System.in);
+        String nombre = "";
+
+
+        System.out.println("¿Cual es el nombre del alumno?");
+        nombre = in.nextLine();
+
+        System.out.println("¿Cual es la nota en la materia Matematicas?");
+        Matematicas=in.nextInt();
+
+        System.out.println("¿Cual es la nota en Biologia?");
+        Biologia= in.nextInt();
+
+        System.out.println("¿Cual es la nota en la materia Quimica?");
+        Quimica=in.nextInt();
+
+        promedio = (Matematicas + Biologia + Quimica) / 3;
+
+
+        if (promedio >= 6 && promedio < 9) {
+            System.out.println("El alumno " + nombre + " aprobo con un promedio de " + promedio);
+
+        } else if (promedio >= 1 && promedio < 6) {
+            System.out.println("El alumno " + nombre + " desaprobo con un promedio de " + promedio);
+        } else if (promedio >=9 && promedio <=10) {
+            System.out.println("El alumno " + nombre + " promociono con un promedio de " + promedio);
+        }
     }
 }
